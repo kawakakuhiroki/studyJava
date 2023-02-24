8a98147742d30209dec90599d2c19b5f3ae6009e
kawakaku <kawakaku.hiroki@icloud.com>
2023-02-24 21:33:16 +0900

    HelloEclipce.java2

--- src/helloeclipce/HelloEclipce.java
+++ src/helloeclipce/HelloEclipce.java
@@ -1,9 +1,8 @@
 package helloeclipce;
 
 public class HelloEclipce {
-
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("HelloEclipseAAABBB");
+		System.out.println("HelloEclipse");
 	}
 }
