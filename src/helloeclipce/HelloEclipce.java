e9087529db0a64cd658c5c949d5ea8599d7135ba
kawakaku <kawakaku.hiroki@icloud.com>
2023-02-25 02:07:08 +0900

    HelloEclipse.java3

--- src/helloeclipce/HelloEclipce.java
+++ src/helloeclipce/HelloEclipce.java
@@ -4,5 +4,10 @@ public class HelloEclipce {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("HelloEclipse");
+		String result = "";
+		for(int i = 0; i < 10; i++) {
+			result =  result + "*";
+		}
+		System.out.println(result);
 	}
 }
