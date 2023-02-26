0d2a09947215274db81498deab08782cd40e8ae0
kawakaku <kawakaku.hiroki@icloud.com>
2023-02-26 17:51:23 +0900

    HellowJavaSilver.java3

--- src/javase11silver/HelloJava11Silver.java
+++ src/javase11silver/HelloJava11Silver.java
@@ -5,5 +5,6 @@ public class HelloJava11Silver {
 	public static void main(String[] args) {
 		System.out.println("HelloJavaSilver");
 		System.out.println("JavaSilver");
+		System.out.println("JavaSilver");
 	}
 }
