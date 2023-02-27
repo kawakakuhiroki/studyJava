6326e3096150b658c73aa13352767fe39a059dc6
kawakaku <kawakaku.hiroki@icloud.com>
2023-02-28 00:55:22 +0900

    Code2_3 add

---
+++ src/sukkiri_java/Code_2_3.java
@@ -0,0 +1,11 @@
+package sukkiri_java;
+
+public class Code_2_3 {
+
+	public static void main(String[] args) {
+		// macのエスケープシーケンスは「¥」ではなく、「\」（option + ¥）
+		System.out.println("私の好きな記号はダブルクォーテーション(\")です");
+		// 「¥」はエスケープシーケンスなしで使用できる
+		System.out.println("¥1,500円");
+	}
+}
---
+++ src/sukkiri_java/package-info.java
@@ -0,0 +1,1 @@
+package sukkiri_java;
