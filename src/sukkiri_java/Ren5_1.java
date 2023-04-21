f12129aa72ceb94216a2417af384f21bd9bb02b5
kawakaku <kawakaku.hiroki@icloud.com>
2023-04-22 00:20:31 +0900

    2023/4/22 00:20

---
+++ src/sukkiri_java/Ren5_1.java
@@ -0,0 +1,15 @@
+package sukkiri_java;
+
+public class Ren5_1 {
+	public static void introduceOneself() {
+		String name = "河角博紀";
+		int age = 35;
+		double tall = 169.9;
+		char eto = '兎';
+		System.out.println("私の名前は" + name + "、年齢は" + age + "歳です。");
+		System.out.println("身長は" + tall + "cm、干支は" + eto + "です。");
+	}
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+}
