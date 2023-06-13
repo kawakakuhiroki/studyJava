836ac9af21b026767b91b7ac7b1c23bb554a2dd4
kawakaku <kawakaku.hiroki@icloud.com>
2023-06-14 04:25:19 +0900

    2023/06/14 04:25

---
+++ src/paiza/D213.java
@@ -0,0 +1,10 @@
+package paiza;
+
+import java.util.*;
+public class D213 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        System.out.println(line * 365);
+    }
+}
