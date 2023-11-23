afe8f7472e1cbc3cdb5419c721972d3067a026dc
kawakaku <kawakaku.hiroki@icloud.com>
2023-11-23 12:25:06 +0900

    2023/11/23 12:24

---
+++ src/javasilver/Code_6_3.java
@@ -0,0 +1,9 @@
+package javasilver;
+
+public class Code_6_3 {
+    public static void main(String[] args) {
+        Object obj = "NULL";
+        System.out.println(obj);
+        System.out.println("test");
+    }
+}
--- src/sukkiri_java/Code6_10.java
+++ src/sukkiri_java/Code6_10.java
@@ -2,9 +2,9 @@ package sukkiri_java;
 
 public class Code6_10 {
 	public static void main(String[] args) {
-		int [] heights = {172,149,152,191,155};
+		int[] heights = { 172, 149, 152, 191, 155 };
 		java.util.Arrays.sort(heights);
-		for(int h : heights) {
+		for (int h : heights) {
 			System.out.println(h);
 		}
 	}
