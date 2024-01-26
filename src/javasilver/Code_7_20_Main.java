13d25f1cff750ffaef247032c29c83fdcd528506
kawakaku <kawakaku.hiroki@icloud.com>
2024-01-26 22:31:05 +0900

    2024/01/24_22:31

---
+++ src/javasilver/Code_7_20_Main.java
@@ -0,0 +1,10 @@
+package javasilver;
+
+public class Code_7_20_Main {
+    public static void main(String[] args) {
+        Code_7_20_A a = new Code_7_20_B();
+        Code_7_20_B b = new Code_7_20_B();
+        a.talk();
+        b.talk();
+    }
+}
